22616edf1b4b67f781b19465bfcc3c51427a5235
aicodingblock <69446789+aicodingblock@users.noreply.github.com>
2021-05-31 17:29:36 +0900

    Update reset.sh

--- blockDriver/reset.sh
+++ blockDriver/reset.sh
@@ -4,5 +4,8 @@ cd /home/pi/blockcoding
 sudo rm -rf kt_ai_makers_kit_block_coding_driver/
 git clone -b release --single-branch https://github.com/aicodingblock/reset_blockdriver.git kt_ai_makers_kit_block_coding_driver
 cd kt_ai_makers_kit_block_coding_driver/blockDriver/
+cp -r /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/etc/desktop/* /home/pi/Desktop/
+sudo chown -R pi:pi /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/amk
+cp -r /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/amk/* /home/pi/ai-makers-kit/python3/
 mkdir key
 npm install
