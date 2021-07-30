ddcf0b158b93e3b93bc51fc58192b811aebda22b
oec <aicodingblock@gmail.com>
2021-07-30 17:09:01 +0900

    update reset_sh...

--- blockDriver/reset.sh
+++ blockDriver/reset.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
+rpi_v3="Raspberry Pi 3 Model B"
+rpi_v3b="Raspberry Pi 3 Model B Plus Rev 1.3"
+rpi_v4="Raspberry Pi 4 Model B"
+
 
 cd /home/pi/blockcoding
 sudo rm -rf kt_ai_makers_kit_block_coding_driver/
-git clone -b release --single-branch https://github.com/aimakers/blockcoding.git kt_ai_makers_kit_block_coding_driver
+git clone -b release --single-branch https://github.com/aicodingblock/reset_blockdriver.git kt_ai_makers_kit_block_coding_driver
 cd kt_ai_makers_kit_block_coding_driver/blockDriver/
+
+if [ "${rpi_v3}" = "Raspberry Pi 3 Model B" ]; then
+  echo "Raspberry Pi 3 Model B"
+  cp /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package_rpi3.json /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package.json
+elif [ "${rpi_v3b}" = "Raspberry Pi 3 Model B Plus Rev 1.3" ]; then
+  echo "Raspberry Pi 3 Model B +"
+  cp /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package_rpi3.json /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package.json
+elif [ "${rpi_v4}" = "Raspberry Pi 4 Model B" ]; then
+  echo "Raspberry Pi 4 Model B"
+  cp /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package_rpi4.json /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/package.json
+else
+  echo "Not Defined Raspberry PI Model ..."
+fi
+
 cp -r /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/etc/desktop/* /home/pi/Desktop/
 sudo chown -R pi:pi /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/amk
 cp -r /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/amk/* /home/pi/ai-makers-kit/python3/
-cp /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/bd_reset.js /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/blockDriver.js
+
+if [ "${rpi_v4}" = "Raspberry Pi 4 Model B" ]; then
+  echo "Copy Raspberry Pi 4 Model B BlockDriver... "
+  cp /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/bd_reset.js /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/blockDriver.js
+
 mkdir key
 npm install
