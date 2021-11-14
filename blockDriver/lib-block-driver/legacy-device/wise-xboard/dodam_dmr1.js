9100b25ddf599638204913e84b18213126e14729
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-11-14 11:54:13 +0900

    fix bug

--- blockDriver/lib-block-driver/legacy-device/wise-xboard/dodam_dmr1.js
+++ blockDriver/lib-block-driver/legacy-device/wise-xboard/dodam_dmr1.js
@@ -2,7 +2,7 @@ const { execQuietlyAsync } = require("../../process-utils")
 
 async function dodam_dmr1(socket, msg, extra) {
     const { l1, r1 } = msg.data ?? {}
-    execQuietlyAsync('python ./dodam_dcmotorspeed1.py', l1, r1)
+    await execQuietlyAsync('python ./dodam_dcmotorspeed1.py', l1, r1)
 }
 
 module.exports = { dodam_dmr1 }
