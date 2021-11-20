e41612c7b7f5738f9a5374a5e5b02934e615ad84
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-11-21 01:03:29 +0900

    socket io cors 적용

--- blockDriver/lib-block-driver/createSocketIoServer.js
+++ blockDriver/lib-block-driver/createSocketIoServer.js
@@ -2,8 +2,8 @@ const { Server } = require('socket.io')
 function createSocketIoServer() {
     const io = new Server({
         cors: {
-            // origin: ['https://aimk.jjfive.net', 'https://aicodingblock.kt.co.kr'],
-            origin: '*',
+            origin: ['https://aicodingblock.kt.co.kr'],
+            // origin: '*', // 이건 안된다
             methods: ["GET", "POST"]
         },
     })
