c68868204c617a63a7813f7b6f7604aa9583b4aa
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-12-02 02:09:56 +0900

    setup script renewal

--- blockDriver/etc/setup/setup-aimk.sh
+++ blockDriver/etc/setup/setup-aimk.sh
@@ -3,13 +3,27 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "${SCRIPT_DIR}"
 
+WORK=/home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver
+ETC=$WORK/blockDriver/etc
+
 ## for serial console
-bash ../systemd/aimk-serial-console-setup.sh 
+bash ${ETC}/systemd/aimk-serial-console-setup.sh 
 
 echo "copy aimk scripts to /usr/local/bin"
 
 ## for /usr/local/bin 
-sudo cp ../usr-local-bin/* /usr/local/bin/
+sudo cp ${ETC}/usr-local-bin/* /usr/local/bin/
 
 ## chmod scripts for aimk prefix
 sudo chmod +x /usr/local/bin/aimk*.sh
+
+# copy desktop files
+echo "copy desktop files"
+sudo cp -r ${ETC}/desktop/* /home/pi/Desktop/
+sudo chown -R pi:pi /home/pi/Desktop/
+
+echo
+echo "=========================="
+echo "aimk script setup finished"
+echo "=========================="
+echo
