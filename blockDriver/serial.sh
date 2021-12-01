db6616928e4b221d817a29bb9142154135ac1149
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-12-02 05:24:45 +0900

    기존의 시리얼 콘솔 시작 스크립트는 더 이상 필요하지 않음

--- blockDriver/serial.sh
+++ blockDriver/serial.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
-sudo systemctl restart serial-getty@ttyUSB0.service
-echo "PC 연결 준비가 완료되었습니다..."
-echo "Enter키를 누르면 종료합니다."
-read choice; case "$choice" in *) exit; esac;
+#!/usr/bin/env bash
+
+echo 
+echo 
+echo "이 프로그램은 더 이상 필요하지 않습니다."
+echo "케이블을 연결하면 자동으로 실행이 됩니다."
+echo "다음 릴리즈에서 제거할 예정입니다."
+echo
+echo
+read _unuse
+exit 0
+
+# sudo systemctl restart serial-getty@ttyUSB0.service
+# echo "PC 연결 준비가 완료되었습니다..."
+# echo "Enter키를 누르면 종료합니다."
+# read choice; case "$choice" in *) exit; esac;
