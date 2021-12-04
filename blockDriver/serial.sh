fe6f8951e29593edab272ccfce2ff125b11c2bf4
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-12-04 09:19:18 +0900

    update serial

--- blockDriver/serial.sh
+++ blockDriver/serial.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-echo 
-echo 
-echo "이 프로그램은 더 이상 필요하지 않습니다."
-echo "케이블을 연결하면 자동으로 실행이 됩니다."
-echo "다음 릴리즈에서 제거할 예정입니다."
+# sudo systemctl restart serial-getty@ttyUSB0.service
+
+# 소리가 나므로 메시지는 표시할 필요 없음
+/usr/local/bin/aimk-button-serial-console.sh toggle
 echo
+echo
+echo
+echo "Enter키를 누르면 종료합니다."
 echo
 read _unuse
-exit 0
 
-# sudo systemctl restart serial-getty@ttyUSB0.service
 # echo "PC 연결 준비가 완료되었습니다..."
 # echo "Enter키를 누르면 종료합니다."
 # read choice; case "$choice" in *) exit; esac;
