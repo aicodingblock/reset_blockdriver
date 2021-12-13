b207c53119c8f5af949b77509d8d6cacafd83821
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-12-13 18:53:24 +0900

    add no-user-gesture-required

--- blockDriver/runBlock.sh
+++ blockDriver/runBlock.sh
@@ -27,5 +27,9 @@ fi
 
 echo "AI 코딩블럭을 시작합니다.";
 
-setsid /usr/bin/chromium-browser --profile-directory=Default ${START_PAGE}  > /dev/null 2>&1 &
+setsid /usr/bin/chromium-browser \
+    --profile-directory=Default \
+    --autoplay-policy=no-user-gesture-required \
+    ${START_PAGE}  > /dev/null 2>&1 &
+
 sudo node /home/pi/blockcoding/kt_ai_makers_kit_block_coding_driver/blockDriver/blockDriver.js
