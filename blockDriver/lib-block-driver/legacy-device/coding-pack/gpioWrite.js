74bc4a6e2b8efbf8de000a9652d0d1231a182677
ktaicoder <ktaicodingblock.dev@gmail.com>
2022-01-10 17:05:33 +0900

    update gpioWrite bug

--- blockDriver/lib-block-driver/legacy-device/coding-pack/gpioWrite.js
+++ blockDriver/lib-block-driver/legacy-device/coding-pack/gpioWrite.js
@@ -1,19 +1,24 @@
 async function gpioWrite(socket, msg, extra) {
-    const { gpio } = extra
-    const { pin, value } = msg.data ?? {}
-    value = value == 1 ? true : false
-    gpio.write(pin, value, function (err) {
+    const { gpio } = extra;
+    const { pin, value = 0} = msg.data ?? {};
+
+    if(typeof pin === 'undefined') {
+        console.log('gpio write invalid param', msg);
+        return
+    }
+
+    gpio.write(pin,  +value === 1, function (err) {
         if (err) {
             //retry
             gpio.setup(pin, gpio.DIR_OUT, function () {
                 gpio.write(pin, value, function (err) {
                     if (err) {
-                        console.log(`gpio write error pin:${pin} value:${value}`)
+                        console.log(`gpio write error pin:${pin} value:${value}`);
                     }
-                })
-            })
+                });
+            });
         }
-    })
+    });
 }
 
-module.exports = { gpioWrite }
+module.exports = { gpioWrite };
